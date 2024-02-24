2c374ceb4610d9ae7ab60ba2db7253161b097550
Austin Drummond <adrum0x7@gmail.com>
2024-02-24 10:45:37 -0500

    craft 3 compatibility

---
+++ src/VersioneerPlugin.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Versioneer plugin for Craft CMS
+ *
+ * A fieldtype for getting at the rest of those entry versions.
+ *
+ * @author    Working Concept
+ * @copyright Copyright (c) 2017 Working Concept
+ * @link      https://workingconcept.com
+ * @package   Versioneer
+ * @since     0.0.1
+ */
+
+namespace workingconcept\versioneer;
+
+use Craft;
+use Craft\base\Plugin as BasePlugin;
+use craft\events\RegisterComponentTypesEvent;
+use craft\services\Fields;
+use workingconcept\versioneer\fieldtypes\VersionHistoryFieldType;
+use yii\base\Event;
+
+class VersioneerPlugin extends BasePlugin
+{
+    public $hasCpSettings = false;
+    public $hasCpSection = false;
+
+    public $version = '0.0.3';
+    public $schemaVersion = '0.0.0';
+
+    public function init()
+    {
+        Event::on(
+            Fields::class,
+            Fields::EVENT_REGISTER_FIELD_TYPES,
+            function (RegisterComponentTypesEvent $event) {
+                $event->types[] = VersionHistoryFieldType::class;
+            }
+        );
+        parent::init();
+    }
+
+    public function getCpNavItem()
+    {
+        return null;
+    }
+}
---
+++ src/fieldtypes/VersionHistoryFieldType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace workingconcept\versioneer\fieldtypes;
+
+use Craft\base\Field as BaseFieldType;
+use Craft;
+use craft\base\ElementInterface;
+
+class VersionHistoryFieldType extends BaseFieldType
+{
+
+	public $name = "yuo";
+
+	public function getName()
+	{
+		return Craft::t('versioneer', 'Version History');
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function displayName(): string
+	{
+		return Craft::t('versioneer', 'Version History');
+	}
+
+	public function getInputHtml($value, ElementInterface $element = null): string
+	{
+		$viewService = Craft::$app->getView();
+
+		$currentSiteHandle = Craft::$app->getSites()->getSiteById($element->siteId, true)->handle;
+		$siteId = $element->siteId ?? Craft::$app->getSites()->getCurrentSite()->id;
+		$html = '';
+
+		$html = $viewService->renderTemplate('versioneer/fieldtypes/history', [
+			'versions' =>  Craft::$app->entryRevisions->getVersionsByEntryId($element->canonicalId, $siteId, 500, true),
+			'entry'    => $element,
+			'id' => $viewService->formatInputId($this->handle),
+			'name' => $this->handle,
+			'value' => $value,
+			'selectedId' => $element->id,
+			'currentSiteHandle' => $currentSiteHandle,
+		]);
+
+		return $html;
+	}
+}
